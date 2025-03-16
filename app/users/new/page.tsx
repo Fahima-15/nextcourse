b8508d627b2a1eb2c3875fe05f054466c2be597f
Amran Mohammed <amranmohammed130@gmail.com>
2025-03-16 15:37:24 +0000

    i have added some css modules and styling

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,23 +1,9 @@
- import { list } from 'postcss';
 import React from 'react'
- 
- interface User {
-id: number;
-name: string;
 
- }
- const NewUserPage  = async  () => {
-   const res = await fetch("https://jsonplaceholder.typicode.com/users ");
-   const users: User [] = await res.json();
-   return (
-     <>
-     <h1>Users</h1>
-     <ul>
-      {/* {users.map(user => <li> . </li>)}  */}
-     </ul>
-     
-     </>
-   )
- }
- 
- export default NewUserPage
+const NewUserPage = () => {
+  return (
+    <div>NewUserPage</div>
+  )
+}
+
+export default NewUserPage
